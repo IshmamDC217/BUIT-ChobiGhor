4f399771fb4eaba027d3fd9d70aa70c4e203ae04
Ishmam Ahmed <ishmam.ahmed24@gmail.com>
2023-07-08 19:25:26 +0100

    Basic structure for landing page

--- components/Homepage.tsx
+++ components/Homepage.tsx
@@ -1,11 +1,22 @@
-import React from 'react'
+import React from 'react';
 
-const Homepage = () => {
+const Homepage: React.FC = () => {
   return (
     <div>
-      
+      <div className="page-wrap">
+        <div className='famcontainer'>
+          <div className='backdrop'>
+            <div className="hero-info">
+              <h2>BUIT</h2>
+              <h1>Nibhrito</h1>
+              <h3>Banani, Dhaka</h3>
+            </div>
+            <div className="hero-image hi-1 backgroundTint"></div>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Homepage
+export default Homepage;
